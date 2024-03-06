ed5f6c06c51bcd96ee0a8bc6973bc61b518b36ce
Sulaiman <151133481+Sulaimannabdul@users.noreply.github.com>
2024-03-07 02:08:11 +0800

    just command

---
+++ command run pip.sh
@@ -0,0 +1,29 @@
+# get pip step-by--stepp in cmd command
+---python
+---pip help
+---py -m pip install -U pygame --user
+---python get-pip.py
+---pip install numpy
+
+#pip--command--cmd
+install                     Install packages.
+download                    Download packages.
+uninstall                   Uninstall packages.
+freeze                      Output installed packages in requirements format.
+inspect                     Inspect the python environment.
+list                        List installed packages.
+show                        Show information about installed packages.
+check                       Verify installed packages have compatible dependencies.
+config                      Manage local and global configuration.
+search                      Search PyPI for packages.
+cache                       Inspect and manage pip's wheel cache.
+index                       Inspect information available from package indexes.
+wheel                       Build wheels from your requirements.
+hash                        Compute hashes of package archives.
+completion                  A helper command used for command completion.
+debug                       Show information useful for debugging.
+help                        Show help for commands.
+
+# more step on VisualSC command--on--terminal
+---
+---
